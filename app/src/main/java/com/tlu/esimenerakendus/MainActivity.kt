72be4ebab220daab5c5de6b86304bc0787b86a20
Mihkel Vaher <mihkelvaher@hotmail.com>
2020-03-03 19:55:10 +0200

    03.03

--- app/src/main/java/com/tlu/esimenerakendus/MainActivity.kt
+++ app/src/main/java/com/tlu/esimenerakendus/MainActivity.kt
@@ -3,6 +3,7 @@ package com.tlu.esimenerakendus
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -14,11 +15,21 @@ class MainActivity : AppCompatActivity() {
         val rollButton: Button = findViewById(R.id.roll_button)
         rollButton.setOnClickListener { rollDice() }
         // Example of a call to a native method
-        sample_text.text = randomDice().toString();
     }
 
     private fun rollDice(){
-        Toast.makeText( this, randomDice().toString(), Toast.LENGTH_SHORT).show()
+//        sample_text.text = randomDice().toString();
+        //Toast.makeText( this, randomDice().toString(), Toast.LENGTH_SHORT).show()
+        val discImage: ImageView = findViewById(R.id.dice_image);
+        val drawResource = when (randomDice()) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        discImage.setImageResource(drawResource);
     }
 
     /**
